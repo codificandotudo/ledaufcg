b036bfa34f381e65706e2397395283929c7cdfd7
thiagsamuel <thiagsamuel@gmail.com>
2013-05-07 03:43:46 +0000

    Created file!

---
+++ LEDA/src/adt/ct/CartesianTreeImpl.java
@@ -0,0 +1,27 @@
+package adt.ct;
+
+import adt.bst.BSTImpl;
+
+public class CartesianTreeImpl <T extends Comparable<T>> extends BSTImpl<T>{
+	
+	public CartesianTreeImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void insert(T element) {
+		// TODO Auto-generated method stub
+	}
+	
+
+	/**
+	 * Retorna o range minimum query do intervalo entre a e b de uma sequencia
+	 * @param a
+	 * @param b
+	 * @return range minimum query do intervalo entre a e b de uma sequencia
+	 */
+	public T rangeMinimumQuery(int a, int b) {
+		return null;
+		// TODO Auto-generated method stub
+	}
+}	
